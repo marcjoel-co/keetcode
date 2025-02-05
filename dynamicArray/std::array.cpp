32b7b36042241aecd349b02151c633eef77e7706
Marc Joel Deus <140829302+marcjoel-co@users.noreply.github.com>
2025-02-05 15:07:19 +0000

    array

---
+++ dynamicArray/std::array.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <array>
+
+using namespace std;
+void printArray(int* array, unsigned int size){
+    
+}
+
+int main(){
+    
+    array<int, 5> data; //std array
+    // array<int, 5> data = {1, 2, 3, 4, 5};
+
+
+
+}
